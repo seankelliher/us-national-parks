9a485869fd7b830836a2af9c310d9effd1d3f4f0
Sean Kelliher <sbkelliher@icloud.com>
2022-07-04 17:28:25 -0400

    Add imports and constants so Apollo will work.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h, provide } from "vue";
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from "@apollo/client/core";
+import gql from "graphql-tag";
 
-createApp(App).mount('#app')
+//This is a Vue Apollo "helper." It provides the apolloClient to the Vue app.
+//Allows you to use queries like "ALL_PARKS_QUERY" in Vue components.
+import { DefaultApolloClient } from "@vue/apollo-composable";
+
+import App from "@/App.vue";
+
+import router from "@/router";
+
+const httpLink = createHttpLink({
+    uri: "http://localhost:4000/server"
+});
+
+//This is the Apollo Client cache, query data stored here.
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+});
+
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient);
+    },
+    render: () => h(App)
+});
+
+app.use(router);
+app.mount("#app");
