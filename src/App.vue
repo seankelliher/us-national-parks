85456df6a96ce56ceaabbbccd3af07b2d337712f
Sean Kelliher <sbkelliher@icloud.com>
2022-07-04 17:27:19 -0400

    Add HeaderPart, NavPart, FooterPart components.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <HeaderPart />
+    <NavPart />
+
+    <router-view />
+    <FooterPart />
 </template>
 
+<script>
+import HeaderPart from "@/components/HeaderPart.vue";
+import NavPart from "@/components/NavPart.vue";
+import FooterPart from "@/components/FooterPart.vue";
+
+//setup is before component is created. It's the entry point for composables.
+//useQuery is a composbale in the Vue Apollo Library.
+//It performs the query, handles loading/error state, returns results to components.
+export default {
+    name: "App",
+    components: {
+        HeaderPart,
+        NavPart,
+        FooterPart
+    }
+    /*setup() {
+    const { result }  = useQuery(ALL_PARKS_QUERY);
+    console.log(result);
+    return { result };
+    }*/
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/style.css";
 </style>
