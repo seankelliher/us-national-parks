73adc614f13d219e0cb50d4ef3e997e039d1ff69
Sean Kelliher <sbkelliher@icloud.com>
2022-07-10 17:18:59 -0400

    Add 'image' and 'caption' to each park object.

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,8 @@
 const parks = [
     {
         id: "acad",
+        image: "acad-little-hunters-beach",
+        caption: "Little Hunters Beach. Photo: Sardius Stalker.",
         name: "Acadia National Park",
         regions: ["Northeast region"],
         states: ["Maine"],
@@ -8,6 +10,8 @@ const parks = [
     },
     {
         id: "arch",
+        image: "arch-delicate-arch",
+        caption: "Delicate Arch. Photo: NPS, Neal Herbert",
         name: "Arches National Park",
         regions: ["Intermountain region"],
         states: ["Utah"],
@@ -15,6 +19,8 @@ const parks = [
     },
     {
         id: "badl",
+        image: "badl-hay-butte-overlook",
+        caption: "Hay Butte Overlook. Photo: NPS, Ed Welsh.",
         name: "Badlands National Park",
         regions: ["Midwest region"],
         states: ["South Dakota"],
@@ -22,6 +28,8 @@ const parks = [
     },
     {
         id: "bibe",
+        image: "bibe-grass-volcanic-peaks",
+        caption: "Grassland and volcanic peaks. Photo: NPS, J. Jurado.",
         name: "Big Bend National Park",
         regions: ["Intermountain region"],
         states: ["Texas"],
@@ -29,6 +37,8 @@ const parks = [
     },
     {
         id: "bisc",
+        image: "bisc-jones-lagoon",
+        caption: "Middle of Jones Lagoon. Photo: Matt Johnson.",
         name: "Biscayne National Park",
         regions: ["Southeast region"],
         states: ["Florida"],
@@ -36,6 +46,8 @@ const parks = [
     },
     {
         id: "blca",
+        image: "blca-east-portal",
+        caption: "Fishing in East Portal. Photo: NPS.",
         name: "Black Canyon of the Gunnison National Park",
         regions: ["Intermountain region"],
         states: ["Colorado"],
@@ -43,6 +55,8 @@ const parks = [
     },
     {
         id: "brca",
+        image: "brca-under-the-rim-trail",
+        caption: "Under-the-Rim Trail. Photo: NPS, Peter Densmore.",
         name: "Bryce Canyon National Park",
         regions: ["Intermountain region"],
         states: ["Utah"],
@@ -50,6 +64,8 @@ const parks = [
     },
     {
         id: "cany",
+        image: "cany-white-rim-road",
+        caption: "White Rim Road. Photo: NPS, Neal Herbert.",
         name: "Canyonlands National Park",
         regions: ["Intermountain region"],
         states: ["Utah"],
@@ -57,6 +73,8 @@ const parks = [
     },
     {
         id: "care",
+        image: "care-cassidy-arch",
+        caption: "Cassidy Arch. Photo: NPS, D. Popovic.",
         name: "Capitol Reef National Park",
         regions: ["Intermountain region"],
         states: ["Utah"],
@@ -64,6 +82,8 @@ const parks = [
     },
     {
         id: "cave",
+        image: "cave-carlsbad-caverns",
+        caption: "Entrance to Carlsbad Caverns. Photo: NPS, Peter Jones.",
         name: "Carlsbad Caverns National Park",
         regions: ["Intermountain region"],
         states: ["New Mexico"],
@@ -71,6 +91,8 @@ const parks = [
     },
     {
         id: "chis",
+        image: "chis-anacapa-island",
+        caption: "Anacapa Island from above. Photo: NPS.",
         name: "Channel Islands National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -78,6 +100,8 @@ const parks = [
     },
     {
         id: "cong",
+        image: "cong-ranger-boardwalk",
+        caption: "Ranger led walk on the boardwalk. Photo: NPS.",
         name: "Congaree National Park",
         regions: ["Southeast region"],
         states: ["South Carolina"],
@@ -85,6 +109,8 @@ const parks = [
     },
     {
         id: "crla",
+        image: "crla-crater-lake-summer",
+        caption: "Crater Lake in summer. Photo: NPS.",
         name: "Crater Lake National Park",
         regions: ["Pacific region"],
         states: ["Oregon"],
@@ -92,6 +118,8 @@ const parks = [
     },
     {
         id: "cuva",
+        image: "cuva-everett-covered-bridge",
+        caption: "Everett Road Covered Bridge. Photo: unknown.",
         name: "Cuyahoga Valley National Park",
         regions: ["Midwest region"],
         states: ["Ohio"],
@@ -99,6 +127,8 @@ const parks = [
     },
     {
         id: "deva",
+        image: "deva-zabriskie-point",
+        caption: "Zabriskie Point at sunrise. Photo: NPS, Kurt Moses.",
         name: "Death Valley National Park",
         regions: ["Pacific region"],
         states: ["California", "Nevada"],
@@ -106,6 +136,8 @@ const parks = [
     },
     {
         id: "dena",
+        image: "dena-eielson-visitor-center",
+        caption: "Near the Eielson Visitor Center. Photo: NPS, Emily Mesner.",
         name: "Denali National Park",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -113,6 +145,8 @@ const parks = [
     },
     {
         id: "drto",
+        image: "drto-coral-monitoring",
+        caption: "Coral monitoring. Photo: NPS, Submerged Resource Center.",
         name: "Dry Tortugas National Park",
         regions: ["Southeast region"],
         states: ["Florida"],
@@ -120,6 +154,8 @@ const parks = [
     },
     {
         id: "ever",
+        image: "ever-everglades-at-sunset",
+        caption: "Everglades sunset. Photo: Sandy Vergara, NPS photo contest.",
         name: "Everglades National Park",
         regions: ["Southeast region"],
         states: ["Florida"],
@@ -127,6 +163,8 @@ const parks = [
     },
     {
         id: "gaar",
+        image: "gaar-caribou-movement",
+        caption: "Caribou movement in spring. Photo: NPS, Alaska Region.",
         name: "Gates of the Arctic National Park & Preserve",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -134,6 +172,8 @@ const parks = [
     },
     {
         id: "jeff",
+        image: "jeff-gateway-arch",
+        caption: "Gateway Arch. Photo: NPS, Sue Ford.",
         name: "Gateway Arch National Park",
         regions: ["Midwest region"],
         states: ["Missouri"],
@@ -141,6 +181,8 @@ const parks = [
     },
     {
         id: "glba",
+        image: "glba-totem-poles",
+        caption: "Totem poles at Bartlett Cove. Photo: NPS, S. Tevebaugh.",
         name: "Glacier Bay National Park & Preserve",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -148,6 +190,8 @@ const parks = [
     },
     {
         id: "glac",
+        image: "glac-road-clearing",
+        caption: "Road clearing on the Upper Slopes. Photo: NPS, Glacier.",
         name: "Glacier National Park",
         regions: ["Intermountain region"],
         states: ["Montana"],
@@ -155,6 +199,8 @@ const parks = [
     },
     {
         id: "grca",
+        image: "grca-dine-woman-on-rim",
+        caption: "A Dine woman in traditional dress. Photo: NPS, M. Quinn.",
         name: "Grand Canyon National Park",
         regions: ["Intermountain region"],
         states: ["Arizona"],
@@ -162,6 +208,8 @@ const parks = [
     },
     {
         id: "grte",
+        image: "grte-garnet-canyon",
+        caption: "The Middle Teton in Garnet Canyon. Photo: NPS.",
         name: "Grand Teton National Park",
         regions: ["Intermountain region"],
         states: ["Wyoming"],
@@ -169,6 +217,8 @@ const parks = [
     },
     {
         id: "grba",
+        image: "grba-great-basin-desert",
+        caption: "Great Basin Desert. Photo: unknown.",
         name: "Great Basin National Park",
         regions: ["Pacific region"],
         states: ["Nevada"],
@@ -176,6 +226,8 @@ const parks = [
     },
     {
         id: "grsa",
+        image: "grsa-dunes-cleveland-peak",
+        caption: "Sand Dunes and Cleveland Peak. Photo: NPS.",
         name: "Great Sand Dunes National Park & Preserve",
         regions: ["Intermountain region"],
         states: ["Colorado"],
@@ -183,6 +235,8 @@ const parks = [
     },
     {
         id: "grsm",
+        image: "grms-headstone-cable-cemetery",
+        caption: "Headstone in Cable Cemetery. Photo: NPS.",
         name: "Great Smoky Mountains National Park",
         regions: ["Southeast region"],
         states: ["North Carolina", "Tennessee"],
@@ -190,6 +244,8 @@ const parks = [
     },
     {
         id: "gumo",
+        image: "gumo-wilderness-ridge",
+        caption: "View from Wilderness Ridge. Photo: unknown.",
         name: "Guadalupe Mountains National Park",
         regions: ["Intermountain region"],
         states: ["Texas"],
@@ -197,6 +253,8 @@ const parks = [
     },
     {
         id: "hale",
+        image: "hale-keoneheehee-trail",
+        caption: "Rangers on Keoneheʻeheʻe Trail. Photo: NPS, Hannah Mousavi.",
         name: "Haleakalā National Park",
         regions: ["Pacific region"],
         states: ["Hawaii"],
@@ -204,6 +262,8 @@ const parks = [
     },
     {
         id: "havo",
+        image: "havo-hikers-kahuku",
+        caption: "Hikers Resting in Kahuku. Photo: NPS, J. Wei.",
         name: "Hawai‘i Volcanoes National Park",
         regions: ["Pacific region"],
         states: ["Hawaii"],
@@ -211,6 +271,8 @@ const parks = [
     },
     {
         id: "hosp",
+        image: "hosp-gulpha-gorge",
+        caption: "Gulpha Gorge Creek. Photo: NPS. Mitch Smith.",
         name: "Hot Springs National Park",
         regions: ["Midwest region"],
         states: ["Arkansas"],
@@ -218,6 +280,8 @@ const parks = [
     },
     {
         id: "indu",
+        image: "indu-cowles-bog",
+        caption: "Wetlands of Cowles Bog. Photo: NPS, K. George.",
         name: "Indiana Dunes National Park",
         regions: ["Midwest region"],
         states: ["Indiana"],
@@ -225,6 +289,8 @@ const parks = [
     },
     {
         id: "isro",
+        image: "isro-feldtmann-tower-view",
+        caption: "View from Feldtmann Tower. Photo: Kaitlyn Knick.",
         name: "Isle Royale National Park",
         regions: ["Midwest region"],
         states: ["Michigan"],
@@ -232,6 +298,8 @@ const parks = [
     },
     {
         id: "jotr",
+        image: "jotr-lost-horse-valley",
+        caption: "Lost Horse Valley. Photo: NPS, Brad Sutton.",
         name: "Joshua Tree National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -239,6 +307,8 @@ const parks = [
     },
     {
         id: "katm",
+        image: "katm-valley-ten-thousand-smokes",
+        caption: "Valley of Ten Thousand Smokes. Photo: NPS, Mike Fitz.",
         name: "Katmai National Park & Preserve",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -246,6 +316,8 @@ const parks = [
     },
     {
         id: "kefj",
+        image: "kefj-exit-glacier",
+        caption: "Exit Glacier. Photo: NPS, S. Wright.",
         name: "Kenai Fjords National Park",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -253,6 +325,8 @@ const parks = [
     },
     {
         id: "kova",
+        image: "kova-camp-site",
+        caption: "Camp site in the Park. Photo: NPS, Arienne Lindholm.",
         name: "Kobuk Valley National Park",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -260,6 +334,8 @@ const parks = [
     },
     {
         id: "lacl",
+        image: "lacl-fireweed-on-beach",
+        caption: "Fireweed on the beach. Photo: NPS, Warren Hill.",
         name: "Lake Clark National Park & Preserve",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -267,6 +343,8 @@ const parks = [
     },
     {
         id: "lavo",
+        image: "lavo-bumpass-hell-boardwalk",
+        caption: "Bumpass Hell boardwalk. Photo: Kirk Barrett.",
         name: "Lassen Volcanic National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -274,6 +352,8 @@ const parks = [
     },
     {
         id: "maca",
+        image: "maca-kayaks-green-river",
+        caption: "Green River. Photo: NPS, Ashley Decker.",
         name: "Mammoth Cave National Park",
         regions: ["Southeast region"],
         states: ["Kentucky"],
@@ -281,6 +361,8 @@ const parks = [
     },
     {
         id: "meve",
+        image: "meve-balcony-house-courtyard",
+        caption: "Balcony House courtyard. Photo: NPS.",
         name: "Mesa Verde National Park",
         regions: ["Intermountain region"],
         states: ["Colorado"],
@@ -288,6 +370,8 @@ const parks = [
     },
     {
         id: "mora",
+        image: "mora-mt-rainer-nisqually-glacier",
+        caption: "Mount Rainier and Nisqually Glacier. Photo: NPS.",
         name: "Mount Rainier National Park",
         regions: ["Pacific region"],
         states: ["Washington"],
@@ -295,6 +379,8 @@ const parks = [
     },
     {
         id: "npsa",
+        image: "npsa-lower-sauma-ridge-trail",
+        caption: "View from the Lower Sauma Ridge Trail. Photo: NPS.",
         name: "National Park of American Samoa",
         regions: ["Pacific region"],
         states: ["American Samoa"],
@@ -302,6 +388,8 @@ const parks = [
     },
     {
         id: "npnh",
+        image: "npnh-great-hall-ellis-island",
+        caption: "The Great Hall at Ellis Island. Photo: NPS.",
         name: "National Parks of New York Harbor",
         regions: ["Northeast region"],
         states: ["New York"],
@@ -309,6 +397,8 @@ const parks = [
     },
     {
         id: "neri",
+        image: "neri-gorge-in-early-fall",
+        caption: "The Gorge in early autumn. Photo: NPS, Dave Bieri.",
         name: "New River Gorge National Park and Preserve",
         regions: ["Northeast region"],
         states: ["West Virginia"],
@@ -316,6 +406,8 @@ const parks = [
     },
     {
         id: "noca",
+        image: "noca-climber-forbidden-ridge",
+        caption: "The West Ridge of Forbidden Peak. Photo: A. Brun.",
         name: "North Cascades National Park",
         regions: ["Pacific region"],
         states: ["Washington"],
@@ -323,6 +415,8 @@ const parks = [
     },
     {
         id: "olym",
+        image: "olym-james-island",
+        caption: "James Island. Photo: NPS, Danielle Archuleta.",
         name: "Olympic National Park",
         regions: ["Pacific region"],
         states: ["Washington"],
@@ -330,6 +424,8 @@ const parks = [
     },
     {
         id: "pefo",
+        image: "pefo-blue-mesa-member",
+        caption: "Blue Mesa Member, Chinle Formation. Photo: NPS.",
         name: "Petrified Forest National Park",
         regions: ["Intermountain region"],
         states: ["Arizona"],
@@ -337,6 +433,8 @@ const parks = [
     },
     {
         id: "pinn",
+        image: "pinn-hillside-in-park",
+        caption: "Pinnacles Rocks. Photo: Emily Novack.",
         name: "Pinnacles National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -344,6 +442,8 @@ const parks = [
     },
     {
         id: "romo",
+        image: "romo-flattop-mountain",
+        caption: "Hiking Flattop Mountain. Photo: NPS, Bonnie Beach.",
         name: "Rocky Mountain National Park",
         regions: ["Intermountain region"],
         states: ["Colorado"],
@@ -351,6 +451,8 @@ const parks = [
     },
     {
         id: "sagu",
+        image: "sagu-tanque-verde-ridge",
+        caption: "Tanque Verde Ridge Trail. Photo: NPS, Ryan Summers.",
         name: "Saguaro National Park",
         regions: ["Intermountain region"],
         states: ["Arizona"],
@@ -358,6 +460,8 @@ const parks = [
     },
     {
         id: "seki",
+        image: "seki-giant-sequoia-tree",
+        caption: "The base of a Giant Sequoia tree. Photo: NPS, Kiel Maddox.",
         name: "Sequoia & Kings Canyon National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -365,6 +469,8 @@ const parks = [
     },
     {
         id: "shen",
+        image: "shen-misty-mountain-view",
+        caption: "A misty winter day. Photo: NPS, E. Knepley.",
         name: "Shenandoah National Park",
         regions: ["Northeast region"],
         states: ["Virginia"],
@@ -372,6 +478,8 @@ const parks = [
     },
     {
         id: "thro",
+        image: "thro-loop-road-in-winter",
+        caption: "The Loop Road in winter. Photo: NPS, Laura Thomas.",
         name: "Theodore Roosevelt National Park",
         regions: ["Midwest region"],
         states: ["North Dakota"],
@@ -379,6 +487,8 @@ const parks = [
     },
     {
         id: "viis",
+        image: "viis-peace-hill-sugar-mill",
+        caption: "Peace Hill Sugar Mill. Photo: NPS, Susanna Pershern.",
         name: "Virgin Islands National Park",
         regions: ["Southeast region"],
         states: ["Virgin Islands"],
@@ -386,6 +496,8 @@ const parks = [
     },
     {
         id: "voya",
+        image: "voya-oberholtzer-trail",
+        caption: "Oberholtzer Trail. Photo: NPS, C. Meridith.",
         name: "Voyageurs National Park",
         regions: ["Midwest region"],
         states: ["Minnesota"],
@@ -393,6 +505,8 @@ const parks = [
     },
     {
         id: "whsa",
+        image: "whsa-dunes-drive",
+        caption: "The curvy Dunes Drive. Photo: NPS.",
         name: "White Sands National Park",
         regions: ["Intermountain region"],
         states: ["New Mexico"],
@@ -400,6 +514,8 @@ const parks = [
     },
     {
         id: "wica",
+        image: "wica-bison-highland-park-creek",
+        caption: "A bison on the trail. Photo: NPS, Bill Schreier.",
         name: "Wind Cave National Park",
         regions: ["Midwest region"],
         states: ["South Dakota"],
@@ -407,6 +523,8 @@ const parks = [
     },
     {
         id: "wotr",
+        image: "wotr-picnic-in-the-meadow",
+        caption: "A picnic in the Meadow. Photo: NPS, Kendra Barat.",
         name: "Wolf Trap National Park for the Performing Arts",
         regions: ["National Capital region"],
         states: ["Virginia"],
@@ -414,6 +532,8 @@ const parks = [
     },
     {
         id: "wrst",
+        image: "wrst-hikers-near-root-glacier",
+        caption: "Hikers near Root Glacier. Photo: NPS.",
         name: "Wrangell - St. Elias National Park & Preserve",
         regions: ["Alaska region"],
         states: ["Alaska"],
@@ -421,6 +541,8 @@ const parks = [
     },
     {
         id: "yell",
+        image: "yell-wolf-in-gardiner-basin",
+        caption: "A wolf in Gardiner Basin. Photo: NPS, Neal Herbert.",
         name: "Yellowstone National Park",
         regions: ["Intermountain region", "Pacific region"],
         states: ["Idaho", "Montana", "Wyoming"],
@@ -428,6 +550,8 @@ const parks = [
     },
     {
         id: "yose",
+        image: "yose-hetch-hetchy",
+        caption: "Hetch Hetchy Reservoir. Photo: Clarisa Flores.",
         name: "Yosemite National Park",
         regions: ["Pacific region"],
         states: ["California"],
@@ -435,13 +559,14 @@ const parks = [
     },
     {
         id: "zion",
+        image: "zion-temples-towers-sunrise",
+        caption: "Temples and Towers at sunrise. Photo: NPS, Rebecca Alfafara.",
         name: "Zion National Park",
         regions: ["Intermountain region"],
         states: ["Utah"],
         website: "https://www.nps.gov/zion/index.htm"
     }
 ];
-
 
 //Resolvers connects everything together.
 //They resolve the “functionalities” defined in the schema.
