e418529b195ad28450e55db76cf4f89477920249
Sean Kelliher <sbkelliher@icloud.com>
2022-07-14 12:41:01 -0400

    Add the HelpBox and MapBox components.

---
+++ src/components/HelpBox.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="help-box">
+        <h3>{{ title }}</h3>
+        <p>{{ text }}</p>
+        <ol>
+            <li
+                v-for="listItem in listItems"
+                :key="listItem"
+            >
+                {{ listItem }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HelpBox",
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        listItems: {
+            type: Array,
+            required: false,
+            default: function() {
+                return [];
+            }
+        }
+    }
+};
+</script>
---
+++ src/components/MapBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="map-box">
+        <figure class="park-image">
+            <img
+                alt="National Parks Service regions map"
+                src="@/assets/images/national-park-service-regions-map.png"
+            >
+            <figcaption>National Park Service Regions. Map produced by Intermountain Region GIS Program Office, September 2003.</figcaption>
+        </figure>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MapBox"
+};
+</script>
