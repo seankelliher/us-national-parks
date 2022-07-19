480797b79ff44a0357887d2d7bce3d19e29739a9
Sean Kelliher <sbkelliher@icloud.com>
2022-07-19 16:53:57 -0400

    Add a route for the NotFound.vue.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from "@/views/HomePage.vue";
 import ParksByName from "@/views/ParksByName.vue";
 import ParksByRegion from "@/views/ParksByRegion.vue";
 import ParksByState from "@/views/ParksByState.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
     {
@@ -24,6 +25,11 @@ const routes = [
         path: "/parks-by-state",
         name: "ParksByState",
         component: ParksByState
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "NotFound",
+        component: NotFound
     }
 ];
 
