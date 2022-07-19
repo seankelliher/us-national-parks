303767bd55faf6a85c68a52124e5486cfcf73c30
Sean Kelliher <sbkelliher@icloud.com>
2022-07-19 16:54:59 -0400

    Add NotFound.vue for 404 results.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <PageIntro
+        title="National Park Service"
+        intro="The National Park Service is the caretaker of America's National Parks."
+    />
+    <section>
+        <PageTitle name="404 - Page Not Found" />
+        <p>Sorry, the page you are looking for does not exist.</p>
+    </section>
+</template>
+
+<script>
+import PageIntro from "@/components/PageIntro.vue";
+
+export default {
+    name: "NotFound",
+    components: {
+        PageIntro
+    }
+};
+</script>
