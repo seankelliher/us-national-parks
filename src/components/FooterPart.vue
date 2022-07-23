46bf1edf77286c1911a851eb7cddf4a4a0e8afbb
Sean Kelliher <sbkelliher@icloud.com>
2022-07-23 16:20:38 -0400

    Add text to footer.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -1,6 +1,12 @@
 <template>
     <footer>
-        <p>This is the footer.</p>
+        <p>
+            Official National Park Service info is at
+            <a
+                href="https://www.nps.gov/index.htm"
+                target="_blank"
+            >nps.gov</a>.
+        </p>
     </footer>
 </template>
 
